89c99e75978fe282e943f5958adcf00cc9707b8b
MayLu <2585036351@qq.com>
2022-08-25 13:41:53 +0800

    初始化项目

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore } from 'vuex'
 
+import cart from './modules/cart'
+import user from './modules/user'
+// vue2.0创建仓库new Vuex.Store({})
+// vue3.0创建仓库createStore({})
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    cart,
+    user
   }
 })
